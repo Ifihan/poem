2ab68506d6c53a9c6222cdff7a847cf545a0a854
Ifihan <victoriaolusheye@gmail.com>
2023-11-14 10:33:58 +0100

    chore: fixed python errors

--- app.py
+++ app.py
@@ -1,54 +1,28 @@
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify, render_template
-from google.generativeai import generate_text
-import requests
+
+import google.generativeai as palm
 import os
 
-app = Flask(__name__)
+load_dotenv()
 
-
-# PALM_API_ENDPOINT = 'https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText'
 API_KEY = os.environ.get('MAKERSUITE_API_KEY')
+palm.configure(api_key=API_KEY)
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/generate-poem', methods=['POST'])
-# def generate_poem():
-#     # Get the prompt from the request data
-#     data = request.get_json()
-#     prompt = data.get('prompt')
-#     poem = generate_text(prompt=prompt)
-
-#     # Return the generated poem as JSON
-#     return jsonify({'poem': poem})
-
-
 @app.route('/generate-poem', methods=['POST'])
 def generate_poem():
-    # Get the prompt from the request data
-    data = request.get_json()
+    data = request.form
     prompt = data.get('prompt')
-    poem = generate_text(prompt=prompt)
-
-    payload = {
-        'prompt': poem,
-    }
-    headers = {
-        'Authorization': f'Bearer {API_KEY}',
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.post(PALM_API_ENDPOINT, json=payload, headers=headers)
-
-    # Check for a successful response and extract the poem
-    if response.status_code == 200:
-        poem_data = response.json()
-        poem = poem_data.get('poem', 'No poem found')
-    else:
-        poem = 'Failed to generate poem'
-
-    return jsonify({'poem': poem})
+    if not prompt:
+        return jsonify({'error': 'Prompt text is required.'})
+    poem = palm.generate_text(prompt=prompt).result
+    return render_template('response.html', poem=poem)
 
 if __name__ == '__main__':
     app.run(debug=True)
